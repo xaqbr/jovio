f299e8a455e4fd668abcf579122e42b05058aba4
Xavier Brown <secton67@gmail.com>
2018-05-29 12:04:28 -0500

    add joi

--- src/server/controllers/script.ts
+++ src/server/controllers/script.ts
@@ -1,10 +1,22 @@
 import { RequestHandler, Router } from "express";
+import { writeFile } from "fs";
+import { promisify } from "util";
+
+const writeFilePromise = promisify(writeFile);
 
 const router = Router();
+
+const createScript: RequestHandler = async (req, res) => {
+    const { name, code } = req.body;
+    if (!name || !code) { res.status(400).send("Name and code fields are required."); }
+    await writeFilePromise(`${name}.class`, code);
+    res.send(201);
+};
+router.post("/", createScript);
 
 const getScript: RequestHandler = (req, res) => {
     res.send(req.body);
 };
-router.post("/", getScript);
+router.get("/", getScript);
 
 export { router };
